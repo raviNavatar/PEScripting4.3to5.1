8ef51227bbf5114c182bd3c03acdf201e6ccc859
Nitin Garg <ngarg@navatargroup.com>
2023-05-03 11:32:18 +0530

    Changes for add Acuity functionality
    Signed-off-by: Nitin Garg <ngarg@navatargroup.com>

--- src/test/java/com/navatar/scripts/Post_CheckScript.java
+++ src/test/java/com/navatar/scripts/Post_CheckScript.java
@@ -634,8 +634,8 @@ public class Post_CheckScript extends BaseLib {
 										CommonLib.ThreadSleep(3000);
 
 										if (edit.verifyAndAddAcuityTabInPages("Navatar Acuity", "Acuity", "Acuity",
-												new String[] { "Z (Do not use) Navatar Clip Edit Utility",
-														"Z (Do not use) Navatar Add Subscribe" },true)) {
+												new String[] { "Z  (Do not use) Navatar Clip Edit Utility",
+														"Z (Do not use) Navatar Add  Subscribe" },true)) {
 											log(LogStatus.INFO, "able to add tab", YesNo.No);
 											CommonLib.ThreadSleep(2000);
 											
